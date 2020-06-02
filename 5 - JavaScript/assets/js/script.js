f4e74946f4bcebf9aa46c65dfe2a40daed449ca7
Ralf Raymer Will <ralfraymer@gmail.com>
2020-06-01 21:23:44 -0300

    Exercício 01
    Exercício 01 JavaScript

---
+++ 5 - JavaScript/assets/js/script.js
@@ -0,0 +1,10 @@
+//Exercio 01
+// Utilize a função prompt para perguntar para o usuário seu nome e idade. Após a leitura, imprima a 
+// string "{nome} tem {idade} anos";
+function ex01(){
+    var nome = prompt('Qual o seu nome?')
+    var idade = prompt('Qual o sua idade?')
+    document.getElementById('nomeIdade').innerHTML = nome + ' tem ' + idade + ' anos';
+}
+
+//Exercício 02
