2b5c966fb91d1f7896ab0292e4c0fdf2efc02d0a
Ralf Raymer Will <ralfraymer@gmail.com>
2020-06-09 12:46:20 -0300

    Exercício 08

--- 5 - JavaScript/assets/js/script.js
+++ 5 - JavaScript/assets/js/script.js
@@ -1,60 +1,75 @@
 //Exercio 01
 // Utilize a função prompt para perguntar para o usuário seu nome e idade. Após a leitura, imprima a 
 // string "{nome} tem {idade} anos";
-function ex01(){
+function ex01() {
     var nome = prompt('Qual o seu nome?')
     var idade = prompt('Qual o sua idade?')
     document.getElementById('nomeIdade').innerHTML = nome + ' tem ' + idade + ' anos';
 }
 
 //Exercício 02
-function ex02(){
+function ex02() {
     var codProd = prompt('Qual o código do produto que você deseja excluir?')
-    if (codProd != null){
-        var confirmar = confirm('Você quer excluir o produto ' + codProd +'?')
-            if(confirmar){
-                alert('Você excluiu o produto ' + codProd)
-            }
+    if (codProd != null) {
+        var confirmar = confirm('Você quer excluir o produto ' + codProd + '?')
+        if (confirmar) {
+            alert('Você excluiu o produto ' + codProd)
+        }
 
     }
 }
 //Exercício 03
-function ex03(){
+function ex03() {
     document.getElementById("tipoTitulos").innerHTML = ""
-    for (let i = 1; i <= 6; i++) {      
-        var para = document.createElement("H"+i)                   
-        var texto = document.createTextNode("Exemplo H"+i)
-        para.appendChild(texto)                                        
-        document.getElementById("tipoTitulos").appendChild(para)    
+    for (let i = 1; i <= 6; i++) {
+        var para = document.createElement("H" + i)
+        var texto = document.createTextNode("Exemplo H" + i)
+        para.appendChild(texto)
+        document.getElementById("tipoTitulos").appendChild(para)
     }
 }
 
 
 //Exercício 04 e Exercício 05
-function excluirProduto(){
+function excluirProduto() {
     ex02();
 }
 
 
 //Exercício 06 
-function calcularIMC(alt, peso){
-    if(!alt & !peso){
+function calcularIMC(alt, peso) {
+    if (!alt & !peso) {
         var alt = prompt('Qual a sua altura?');
         var peso = prompt('Qual o seu peso?');
     }
 
     var imc = peso / (alt * alt);
     imc = parseFloat(imc.toFixed(2))
-    alert("O seu IMC é de: "+ imc);
+    alert("O seu IMC é de: " + imc);
 }
 
 //Exercício 07
-function capturaInputIMC(){
+function capturaInputIMC() {
     var altura = document.getElementById('altura').value;
     var peso = document.getElementById('peso').value;
-    if(peso != "" & altura != ""){
+    if (peso != "" & altura != "") {
         calcularIMC(altura, peso)
-    }else{
+    } else {
         alert("Digite a altura/Peso!")
+    }
+}
+
+//Exercício 08
+function continentePais() {
+    var continentes = [
+        ['Americano', 'Brasil', 'EUA', 'Canadá', 'Uruguai'],
+        ['Europeu', 'Itália', 'Irlanda', 'França'],
+        ['Asiático', 'China', 'Japão']
+    ]
+    for (let i = 0; i < continentes.length; i++) {
+        console.log(continentes[i][0] + ':')
+        for (let j = 1; j < continentes[i].length; j++) {
+            console.log(continentes[i][j])
+        }
     }
 }
